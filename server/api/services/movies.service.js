c1ce5c14cca1148361fdfccffd6dd5ff66f2cf2d
RazzM13 <34776591+RazzM13@users.noreply.github.com>
2019-12-21 21:34:43 +0200

    Commented find method of "server/api/services/movies.service.js".

--- server/api/services/movies.service.js
+++ server/api/services/movies.service.js
@@ -2,6 +2,13 @@ import l from '../../common/logger';
 import db from './movies.db.service';
 
 class MoviesService {
+  /**
+   * Provides the ability to search movie suggestions based on a film name via fuzzy matching and to sort the results in
+   * descending order based on audience_score or the rotten_tomatoes scores.
+   * @param {string} q Film name that will be used for fuzzy matching.
+   * @param {string} sortBy Sorting column one of audience_score or, the default, rotten_tomatoes.
+   * @returns {*}
+   */
   find(q, sortBy = 'rotten_tomatoes') {
     l.info(`${this.constructor.name}.find(${q}, ${sortBy})`);
     return db.find(q, sortBy);
